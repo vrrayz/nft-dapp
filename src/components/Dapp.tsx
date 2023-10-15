ccdab87ba18aba64992b92e6f299b626af7cf80a
Victor <emmyvic98@gmail.com>
2023-10-15 11:16:14 +0100

    user create event and mint nft

--- src/components/Dapp.tsx
+++ src/components/Dapp.tsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
 
-import { JsonRpcSigner, ethers } from "ethers";
+import { ContractFactory, JsonRpcSigner, ethers } from "ethers";
+import { abi } from "../data/abi";
+import { bytecode } from "../data/bytecode";
+import { SEPOLIA_NETWORK } from "../data/network";
+
+interface EventDetails {
+  name?: string;
+  symbol?: string;
+  description?: string;
+}
 
 export const Dapp = () => {
   const [signer, setSigner] = useState<JsonRpcSigner>();
   const [isUserConnected, setIsUserConnected] = useState<boolean>(false);
+  const [nftDetails, setNftDetails] = useState<EventDetails>({});
   const connectWallet = async () => {
     try {
       let provider;
@@ -19,7 +29,7 @@ export const Dapp = () => {
         // Connect to the MetaMask EIP-1193 object. This is a standard
         // protocol that allows Ethers access to make all read-only
         // requests through MetaMask.
-        provider = new ethers.BrowserProvider(window.ethereum);
+        provider = new ethers.BrowserProvider(window.ethereum, SEPOLIA_NETWORK);
 
         // It also provides an opportunity to request access to write
         // operations, which will be performed by the private key
@@ -31,9 +41,28 @@ export const Dapp = () => {
     }
   };
 
+  const createNft = async () => {
+    const contractFactory = new ContractFactory(abi,bytecode,signer)
+    const contract = await contractFactory.deploy(nftDetails.name, nftDetails.symbol, nftDetails.description)
+    // The contract address comes out from contract.target
+    console.log('The contract address === ',await contract.getAddress())
+    console.log("The nft details ", nftDetails);
+  };
+
   useEffect(() => {
     signer?.address && setIsUserConnected(true);
+    console.log("The user address is ", signer?.address);
   }, [signer]);
+
+  const shouldDisable = (eventDetails: EventDetails) =>
+    !(eventDetails.name && eventDetails.name.length >= 4 && eventDetails.symbol && eventDetails.description);
+
+  const setEventDetails = (eventDetails: EventDetails) => {
+    setNftDetails(eventDetails);
+    if (eventDetails.name && eventDetails.name.length >= 4) {
+      eventDetails.symbol = eventDetails.name.substring(0, 4);
+    }
+  };
 
   return (
     <div>
@@ -44,14 +73,32 @@ export const Dapp = () => {
           <div>
             <label htmlFor="event-name">Event Name</label>
             <br />
-            <input type="text" placeholder="Enter Event Name" />
+            <input
+              type="text"
+              placeholder="Enter Event Name"
+              onChange={(e) =>
+                setEventDetails({ ...nftDetails, name: e.target.value })
+              }
+            />
           </div>
           <div>
             <label htmlFor="event-description">Description</label>
             <br />
-            <textarea id="event-description" cols={30} rows={30}></textarea>
+            <textarea
+              id="event-description"
+              cols={30}
+              rows={30}
+              onChange={(e) =>
+                setEventDetails({ ...nftDetails, description: e.target.value })
+              }
+            ></textarea>
           </div>
-          <button>Mint</button>
+          <button
+            onClick={() => createNft()}
+            disabled={shouldDisable(nftDetails)}
+          >
+            Mint
+          </button>
         </section>
       )}
     </div>
---
+++ src/data/network.ts
@@ -0,0 +1,7 @@
+export const SEPOLIA_NETWORK = {
+    name: 'Sepolia test network',
+    chainId: 11155111,
+    // //layerOneConnection?: Provider,
+    // ensAddress?: string,
+    // ensNetwork?: number
+}
